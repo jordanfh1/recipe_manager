5cdf034b4288e9943ceb268028836f266d20b8c0
Jordan <jordanfhoughton@gmail.com>
2025-06-02 10:20:08 +0100

    fixes typo err in recipe routes

--- recipe_manager_app/backend/routes/recipeRoutes.js
+++ recipe_manager_app/backend/routes/recipeRoutes.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const Recipe = require('../modeels/Recpipe');
+const Recipe = require('../models/Recpipe');
 
 // get all recipes
 router.get('/', async (req, res) => {
